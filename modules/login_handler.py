dbc9eccffd3ecebfd0357ddd13354ba8ccf80828
Yiğitcan BAŞALMA <yigit.basalma@gmail.com>
2018-05-05 13:33:30 +0300

    Started to develop management page.

--- modules/login_handler.py
+++ modules/login_handler.py
@@ -27,7 +27,7 @@ class Protector(Db):
         password = calculate_hash(password, method="sha256")
         session_environ = ["UID", "FIRSTNAME", "LASTNAME", "EMAIL", "MAJORITY", "COUNTRY", "STATUS", "ROLE", "CITY", "HOSPITAL", "PROJECT"]
         try:
-            user_data = get_users_table(where="EMAIL='" + email + "' AND PASSWORD='" + password + "'", column="ID,F_NAME,L_NAME,EMAIL,MAJORITY,COUNTRY,STATUS,ROLE,CITY,HOSPITAL,PROJECT")[0]
+            user_data = get_users_table(where="EMAIL='" + email + "' AND PASSWORD='" + password + "' AND STATUS='Enabled'", column="ID,F_NAME,L_NAME,EMAIL,MAJORITY,COUNTRY,STATUS,ROLE,CITY,HOSPITAL,PROJECT")[0]
         except IndexError:
             user_data = tuple()
         if len(user_data) > 0:
