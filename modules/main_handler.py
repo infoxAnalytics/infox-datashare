a6f5d5c8232e214684eb3221b85c6af734338797
Yiğitcan BAŞALMA <yigit.basalma@gmail.com>
2018-05-05 14:43:44 +0300

    Started to develop management page.

--- modules/main_handler.py
+++ modules/main_handler.py
@@ -153,7 +153,7 @@ class Processor(Db):
                 if args["PROJECT"][0] == "none":
                     return response_create(json.dumps({"STATUS": "error", "ERROR": "Project is not none for this process."}))
                 self.write_mysql("UPDATE users SET STATUS='Enabled', PROJECT='{0}' WHERE ID='{1}'".format(",".join([self.get_project_name(i) for i in args["PROJECT"] if i != "none"]), args["USER_ID"]))
-            log = "User status changed by \"{0} {1}\".Status: {2}, Projects: {3}.".format(f_name, l_name, args["USER_STATUS"], ",".join(args["PROJECT"]))
+            log = "User status changed by \"{0} {1}\".Status: {2}, Projects: {3}.".format(f_name, l_name, args["USER_STATUS"].capitalize(), ",".join([self.get_project_name(i) for i in args["PROJECT"] if i != "none"]))
             write_log_to_mysql(event_type, ip, "INFO", log, self.system_username)
             self.mysql_commit()
             return response_create(json.dumps({"STATUS": "OK", "MESSAGE": "Status changed."}))
